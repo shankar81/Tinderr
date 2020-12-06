906ce55c9501a4a5236c39bc388497cd08670813
shankarsawant81 <shankarsawant81@gmail.com>
2020-12-06 13:40:26 +0530

    1. Set up initial navigation component. 2. Working on AuthFragment. 3. Added app icon.

---
+++ app/src/main/java/com/example/tinderr/AuthFragment.kt
@@ -0,0 +1,21 @@
+package com.example.tinderr
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AuthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+}
+
+
+
