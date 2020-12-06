d638345220174f640df98ef11e231411d03aa2fc
shankarsawant81 <shankarsawant81@gmail.com>
2020-12-06 18:08:06 +0530

    Done - Auth with phone number

---
+++ app/src/main/java/com/example/tinderr/AuthNumberFragment.kt
@@ -0,0 +1,28 @@
+package com.example.tinderr
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AuthNumberFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_auth_number, container, false)
+
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+
+}
